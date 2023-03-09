c3941bf3c6dadb2b6084121aa2094c087d5983ed
akthrms <akt.hrms@gmail.com>
2023-03-10 00:55:57 +0900

    feat: insertを追加

--- src/internal/models/snippets.go
+++ src/internal/models/snippets.go
@@ -17,8 +17,27 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-func (m *SnippetModel) Insert(title string, content string, expires time.Time) (int, error) {
-	return 0, nil
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	stmt := `
+	insert into snippets (title, content, created, expires) values (
+		?,
+		?,
+		utc_timestamp(),
+		date_add(utc_timestamp(), interval ? day)
+	)
+	`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
